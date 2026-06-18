eb3e377b2ffb39048ed5c083742883a0fe785274
SabinaYesmin371 <yesminmstsabina19@gmail.com>
2026-06-19 02:15:35 +0530

    update: H2 database implemented

--- purchase-transaction/src/test/java/com/wex/purchasetransaction/controller/PurchaseControllerTest.java
+++ purchase-transaction/src/test/java/com/wex/purchasetransaction/controller/PurchaseControllerTest.java
@@ -15,10 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -29,14 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@Testcontainers
 public class PurchaseControllerTest {
-
-    @Container
-    public static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("testdb")
-            .withUsername("test")
-            .withPassword("test");
 
     private static WireMockServer wireMockServer;
 
@@ -48,10 +37,6 @@ public class PurchaseControllerTest {
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
         registry.add("treasury.api.url", () -> wireMockServer.baseUrl());
     }
 
